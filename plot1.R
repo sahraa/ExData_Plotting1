8a43af29bd33904006a75f06a3ca35f5b74406ca
YalDa <yalda.amini89@gmail.com>
2016-06-01 12:36:14 +0430

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -9,6 +9,6 @@ PD <- read.table(HHF, header=T, sep=";", na.strings="?")
 FD <- PD[PD$Date %in% c("1/2/2007","2/2/2007"),]
 SetTime <-strptime(paste(FD$Date, FD$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
 FD <- cbind(SetTime, FD)
-png("plot2.png",width = 480, height = 480)
-plot(FD$SetTime, FD$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
+png("plot1.png",width = 480, height = 480)
+hist(FD$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
